37305ca73fe7cbf5b642d2b8471bf117d5a5337a
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-15 12:10:26 +0200

    dev: added nodemon and serve-index

--- back/server.js
+++ back/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const www = process.env.WWW_DIR || "./public";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
 });
+
+app.use(express.static(www));
+app.use(serveIndex(www, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
