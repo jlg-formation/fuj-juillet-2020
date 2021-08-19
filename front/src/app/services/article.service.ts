90ec139b601655d327c2a66038b2292450da242a
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-19 17:00:55 +0200

    dev: added localstorage for offline

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,16 +5,29 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Marteau', price: 11, qty: 4567 },
-    { name: 'Tondeuse à gazon', price: 234, qty: 3 },
-    { name: 'Pelle', price: 1.23, qty: 5 },
-  ];
+  articles: Article[] = this.getArticles();
 
   constructor() {}
 
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis', price: 2.34, qty: 123 },
+        { name: 'Marteau', price: 11, qty: 4567 },
+        { name: 'Tondeuse à gazon', price: 234, qty: 3 },
+        { name: 'Pelle', price: 1.23, qty: 5 },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
+
+  save() {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
+
   add(article: Article) {
     this.articles.push(article);
+    this.save();
   }
 }
