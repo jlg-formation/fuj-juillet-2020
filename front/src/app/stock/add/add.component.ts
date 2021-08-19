b912a25d0397d746a1a4abf2c51a11851b12d82e
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-19 15:59:15 +0200

    dev: added reactive form

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('toto', [Validators.required]),
+    price: new FormControl(1.23, [Validators.required]),
+    qty: new FormControl(1, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit(): void {
+    console.log('submit');
+    this.router.navigate(['..'], { relativeTo: this.route });
   }
-
 }
--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -5,17 +5,15 @@ import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { AddComponent } from './add/add.component';
 import { WidgetModule } from '../widget/widget.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    StockComponent,
-    AddComponent
-  ],
+  declarations: [StockComponent, AddComponent],
   imports: [
     CommonModule,
     StockRoutingModule,
-    WidgetModule
-  ]
+    WidgetModule,
+    ReactiveFormsModule,
+  ],
 })
-export class StockModule { }
+export class StockModule {}
