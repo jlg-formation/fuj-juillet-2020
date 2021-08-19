8127351173e4853e6c5ddaf58e4745fbacaa0b40
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-19 15:29:42 +0200

    dev: onDestroy

--- front/src/app/widget/autofocus.directive.ts
+++ front/src/app/widget/autofocus.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',
 })
-export class AutofocusDirective implements OnInit {
+export class AutofocusDirective implements OnInit, OnDestroy {
   constructor(private elt: ElementRef<HTMLElement>) {
     console.log('directive autofocus instantiated.', elt);
   }
 
   ngOnInit(): void {
     this.elt.nativeElement.focus();
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy autofocus directive');
   }
 }
