940375ec54b29b75da50cf1b4889c77252d10b86
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-20 11:01:06 +0200

    dev: export from html

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Article } from '../interfaces/article';
 
 import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-stock',
@@ -31,8 +32,18 @@ export class StockComponent implements OnInit {
 
   export() {
     console.log('export');
-    const doc = new jsPDF();
-    doc.text('Jspdf text!', 10, 10);
-    doc.save('test.pdf');
+    let DATA = document.querySelector('table') as HTMLElement;
+
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 100;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+
+      PDF.save('angular-demo.pdf');
+    });
   }
 }
