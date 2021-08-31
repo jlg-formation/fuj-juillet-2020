4b1073ba30db834ba895b6ef7f6c2b32b595ef2f
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-31 12:03:23 +0200

    dev: server refactored

--- back/server.ts
+++ back/server.ts
@@ -1,32 +1,8 @@
-import express from "express";
-import serveIndex from "serve-index";
-import cors from "cors";
+import { WebServer } from "./src/WebServer";
 
-import { articleRouter } from "./articles.router";
-
-const app = express();
-const port = process.env.PORT || 3000;
-const www = process.env.WWW_DIR || "./public";
-
-app.use(express.json());
-app.use(cors());
-
-app.use((req, res, next) => {
-  console.log("req: ", req.url);
-  next();
-});
-
-app.use("/api/articles", articleRouter);
-
-app.get("/api/date", (req, res) => {
-  res.json({
-    date: new Date(),
+(async () => {
+  const server = new WebServer({
+    port: +(process.env.WEBSERVER_PORT || 3000),
   });
-});
-
-app.use(express.static(www));
-app.use(serveIndex(www, { icons: true }));
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  await server.start();
+})();
