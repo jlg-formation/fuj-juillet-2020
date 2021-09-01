628a84f8a0ccba838e013c147054c800a97a9c8d
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-01 18:12:36 +0200

    dev: add the route user/login

--- front/src/app/user/user-routing.module.ts
+++ front/src/app/user/user-routing.module.ts
@@ -1,11 +1,15 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
--- front/src/app/user/user.module.ts
+++ front/src/app/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
