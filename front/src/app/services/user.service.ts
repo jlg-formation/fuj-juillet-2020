46533da7e4f5f8dd1f0df35228aa9117958f0f8e
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-01 16:54:45 +0200

    dev: user service

--- front/src/app/services/user.service.ts
+++ front/src/app/services/user.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { User } from '../interfaces/user';
 
@@ -9,5 +11,28 @@ import { User } from '../interfaces/user';
 export class UserService {
   user$ = new BehaviorSubject<User | undefined>(undefined);
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.checkConnection(false);
+  }
+
+  checkConnection(isLazy = false): void {
+    if (isLazy && this.user$.value) {
+      return;
+    }
+
+    this.http
+      .get<User | undefined>('/api/auth/isConnected')
+      .pipe(
+        map((u) => {
+          console.log('isConnected user body', u);
+          return u || undefined;
+        }),
+        catchError((err) => of(undefined))
+      )
+      .subscribe({
+        next: (user) => {
+          this.user$.next(user);
+        },
+      });
+  }
 }
