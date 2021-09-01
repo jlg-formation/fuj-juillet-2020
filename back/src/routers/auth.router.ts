310762950a0fb765d18effd68432683d0762feb8
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-01 17:46:10 +0200

    dev: added auth router

---
+++ back/src/routers/auth.router.ts
@@ -0,0 +1,38 @@
+import {NextFunction, Request, Response, Router} from 'express';
+import {User} from '../interfaces/User';
+
+declare module 'express-session' {
+  interface SessionData {
+    accessToken?: string;
+    afterLoginRoute?: string;
+    user?: User;
+  }
+}
+
+declare module 'http' {
+  interface IncomingMessage {
+    user: User;
+  }
+}
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session.user) {
+    console.log('not connected.');
+    return res.status(401).end();
+  }
+  const user = req.session.user;
+  req.user = user;
+  next();
+};
+
+const app = Router();
+
+export const authRouter = app;
+
+app.get('/isConnected', (req, res) => {
+  if (!req.session.user) {
+    console.log('not connected.');
+    return res.status(401).end();
+  }
+  return res.json(req.session.user);
+});
