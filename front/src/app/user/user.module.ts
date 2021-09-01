848f601d39e8a9ca7088aea2d3e7ee09bdd2645a
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-01 18:14:57 +0200

    dev: adding html to login

--- front/src/app/user/user.module.ts
+++ front/src/app/user/user.module.ts
@@ -1,3 +1,4 @@
+import { WidgetModule } from './../widget/widget.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,15 +6,8 @@ import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { LoginComponent } from './login/login.component';
 
-
 @NgModule({
-  declarations: [
-    UserComponent,
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [UserComponent, LoginComponent],
+  imports: [CommonModule, UserRoutingModule, WidgetModule],
 })
-export class UserModule { }
+export class UserModule {}
