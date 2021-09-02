d8bddaee3326c7bc7e3d3d6897883428f46f1934
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-02 11:25:34 +0200

    dev: connect with the login page
    Note: I do that for future use of provider
    choice: github, azure ad, google, etc.

--- front/src/app/user/login/login.component.ts
+++ front/src/app/user/login/login.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Oauth2Service } from '../../services/oauth2.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(public oauth2Service: Oauth2Service) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
