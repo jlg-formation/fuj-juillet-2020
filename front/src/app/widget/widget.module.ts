6142ee9473bf9ddf6f37bef7fd66185caca95c12
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-03 11:08:31 +0200

    dev: add details component

--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutofocusDirective } from './autofocus.directive';
 import { UserStatusComponent } from './user-status/user-status.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
-  declarations: [AutofocusDirective, UserStatusComponent],
+  declarations: [AutofocusDirective, UserStatusComponent, DetailComponent],
   imports: [CommonModule, RouterModule],
-  exports: [AutofocusDirective, UserStatusComponent],
+  exports: [AutofocusDirective, UserStatusComponent, DetailComponent],
 })
 export class WidgetModule {}
