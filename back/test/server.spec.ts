6bd91f22a673b0907e310a1dd84576b4a72c369c
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-09 16:54:00 +0200

    test: updated url to /api/date

--- back/test/server.spec.ts
+++ back/test/server.spec.ts
@@ -1,15 +1,15 @@
 import assert from 'assert';
-import {WebServer} from '../src/WebServer';
 import got from 'got';
+import {WebServer} from '../src/WebServer';
 
 describe('Server', () => {
   it('should start and stop', async function () {
     this.timeout(15000);
     const server = new WebServer({port: 3400});
     await server.start();
-    const data = await got.get('http://localhost:3400/api/articles').json();
+    const data = await got.get('http://localhost:3400/api/date').json();
     console.log('data: ', data);
     await server.stop();
-    assert(data instanceof Array);
+    assert(data instanceof Object);
   });
 });
