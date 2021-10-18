32923da03e93630c1a7b163dc5d738621596357d
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-18 15:01:11 +0200

    dev: autofocus well tested.

--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,28 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { AutofocusDirective } from './autofocus.directive';
 
+@Component({
+  selector: 'app-test',
+  template: `<div>
+    <input type="text" name="truc" appAutofocus />
+  </div> `,
+})
+export class TestComponent {}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new AutofocusDirective();
-    // expect(directive).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AutofocusDirective, TestComponent],
+    }).compileComponents();
+  });
+
+  it('should autofocus on html content', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const inputElt = compiled.querySelector('input');
+    const focusElt = document.activeElement;
+    expect(inputElt === focusElt).toBeTrue();
   });
 });
