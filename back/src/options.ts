67ed5791b45bf42f900556a6e3070dd3b9a979c0
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-03 17:11:46 +0100

    dev: refactored options

--- back/src/options.ts
+++ back/src/options.ts
@@ -2,4 +2,5 @@ import {WebServerOptions} from './interfaces/WebServerOptions';
 
 export const options: Partial<WebServerOptions> = {
   port: +(process.env.WIAME_NODESERVER_PORT || 3000),
+  dbUri: process.env.WIAME_DB_URI || 'mongodb://localhost/gestion-stock',
 };
