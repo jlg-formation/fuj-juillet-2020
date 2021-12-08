5b77e5655bd6182710a76d3bc2f0bda03c748328
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-08 19:15:39 +0100

    dev: refactor adding

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -10,6 +10,8 @@ import { Article } from 'src/app/interfaces/article';
   styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  isAdding = false;
+  error = '';
   f = new FormGroup({
     name: new FormControl('toto', [Validators.required]),
     price: new FormControl(1.23, [Validators.required]),
@@ -25,8 +27,18 @@ export class AddComponent implements OnInit {
   ngOnInit(): void {}
 
   submit(): void {
-    console.log('submit');
-    this.articleService.add(this.f.value as Article);
-    this.router.navigate(['..'], { relativeTo: this.route });
+    (async () => {
+      try {
+        console.log('submit');
+        this.isAdding = true;
+        await this.articleService.add(this.f.value as Article).toPromise();
+
+        this.router.navigate(['..'], { relativeTo: this.route });
+        this.isAdding = false;
+      } catch (err) {
+        this.isAdding = false;
+        this.error = (err as Error).message;
+      }
+    })();
   }
 }
