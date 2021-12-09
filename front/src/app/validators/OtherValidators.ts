0e1945c3bd8972adb13bee912129fa3873d2b836
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-09 19:00:47 +0100

    dev: integer validator

--- front/src/app/validators/OtherValidators.ts
+++ front/src/app/validators/OtherValidators.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class OtherValidators {
   static integer(control: AbstractControl): ValidationErrors | null {
