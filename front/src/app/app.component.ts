7d50ddadd58ba7d29f17c5cbabceb7497f563cb7
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-10 11:02:19 +0100

    dev: test with library and fake service

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { JlgService } from 'jlg';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  constructor(private jlgService: JlgService) {}
 }
