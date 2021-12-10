f9c44eafcb27cfa355faeac29603d0a671156c24
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-10 12:18:32 +0100

    dev: removed interceptor definition

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CredentialsInterceptor } from './interceptors/credentials.interceptor';
+import { CredentialsInterceptor } from '@jlguenego/angular-tools';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
