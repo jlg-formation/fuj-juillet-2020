3f42dbfccca35d16bae618e8b0694720bb844420
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-15 11:48:14 +0100

    dev: remove autofocus

--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -1,25 +1,14 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AutofocusDirective } from './autofocus.directive';
 import { UserStatusComponent } from './user-status/user-status.component';
 import { DetailComponent } from './detail/detail.component';
 import { FieldErrorComponent } from './field-error/field-error.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AutofocusDirective,
-    UserStatusComponent,
-    DetailComponent,
-    FieldErrorComponent,
-  ],
+  declarations: [UserStatusComponent, DetailComponent, FieldErrorComponent],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
-  exports: [
-    AutofocusDirective,
-    UserStatusComponent,
-    DetailComponent,
-    FieldErrorComponent,
-  ],
+  exports: [UserStatusComponent, DetailComponent, FieldErrorComponent],
 })
 export class WidgetModule {}
