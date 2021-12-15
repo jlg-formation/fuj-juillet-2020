a18acbc73f42fea2035d688a082a3b38704f3e79
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-15 13:45:33 +0100

    refactor: organize import (alt_shift_o)

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -1,10 +1,10 @@
-import { WidgetModule } from './../widget/widget.module';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { WidgetModule } from './../widget/widget.module';
 import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, BodyComponent],
