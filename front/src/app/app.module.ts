1561208aac7313777cbdd2df08e01a15447ea163
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-16 12:36:10 +0100

    dev: integrated crudService

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CredentialsInterceptor } from '@jlguenego/angular-tools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CredentialsInterceptor } from '@jlguenego/angular-tools';
 import { LayoutModule } from './layout/layout.module';
+import { Error403Component } from './routes/error403/error403.component';
+import { Error404Component } from './routes/error404/error404.component';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
-import { ArticleService } from './services/article.service';
-import { HttpArticleService } from './services/http-article.service';
-import { Error404Component } from './routes/error404/error404.component';
-import { Error403Component } from './routes/error403/error403.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +20,6 @@ import { Error403Component } from './routes/error403/error403.component';
   ],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, HttpClientModule],
   providers: [
-    { provide: ArticleService, useClass: HttpArticleService },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: CredentialsInterceptor,
--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,48 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, BehaviorSubject } from 'rxjs';
-import { delay, tap } from 'rxjs/operators';
+import { CrudService } from '@jlguenego/angular-tools';
 import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ArticleService {
-  articles$ = new BehaviorSubject<Article[]>([]);
-
-  constructor() {
-    this.articles$.subscribe((articles) => {
-      localStorage.setItem('articles', JSON.stringify(articles));
-    });
-  }
-
-  add(article: Article): Observable<void> {
-    const articles = [...this.articles$.value, article];
-    this.articles$.next(articles);
-    return of(undefined);
-  }
-
-  getArticles(): Article[] {
-    const str = localStorage.getItem('articles');
-    if (!str) {
-      return [];
-    }
-    return JSON.parse(str) as Article[];
-  }
-
-  remove(selectedArticles: Set<Article>): Observable<void> {
-    const articles = this.articles$.value.filter(
-      (a) => !selectedArticles.has(a)
-    );
-    this.articles$.next(articles);
-    return of(undefined).pipe(delay(2000));
-  }
-
-  retrieveAll(): Observable<void> {
-    return of(undefined).pipe(
-      delay(2000),
-      tap(() => {
-        this.articles$.next(this.getArticles());
-      })
-    );
+export class ArticleService extends CrudService<Article> {
+  getEndpoint(): string {
+    return '/api/articles';
   }
 }
