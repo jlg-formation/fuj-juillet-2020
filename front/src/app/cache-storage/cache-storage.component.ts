908858878f32ec026dc9945b13ff22231bcfaa86
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-03 19:31:07 +0100

    dev: cache storage management list

--- front/src/app/cache-storage/cache-storage.component.ts
+++ front/src/app/cache-storage/cache-storage.component.ts
@@ -1,6 +1,7 @@
-import { ArticleService } from './../services/article.service';
 import { Component } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+
+// const KEY = 'uploads:cache';
+const KEY = 'jlg';
 
 @Component({
   selector: 'app-cache-storage',
@@ -8,28 +9,17 @@ import { lastValueFrom } from 'rxjs';
   styleUrls: ['./cache-storage.component.scss'],
 })
 export class CacheStorageComponent {
-  constructor(public articleService: ArticleService) {}
-  addImages() {
+  cacheUrls: string[] = [];
+  constructor() {
     (async () => {
-      try {
-        console.log('adding all articles images');
-        const articles = this.articleService.documents$.value;
-        console.log('articles: ', articles);
-        if (articles.length === 0) {
-          await lastValueFrom(this.articleService.retrieveAll());
-        }
-
-        const cache = await window.caches.open('jlg');
-        articles.forEach((a) => {
-          console.log('a.image: ', a.image);
-          if (!a.image) {
-            return;
-          }
-          cache.add(a.image);
-        });
-      } catch (err) {
-        console.error('err: ', err);
+      const keys = await window.caches.keys();
+      const key = keys.find((k) => k.match(KEY));
+      if (!key) {
+        console.log('no key found.');
+        return;
       }
+      const cache = await window.caches.open(key);
+      this.cacheUrls = (await cache.keys()).map((r) => r.url);
     })();
   }
 }
