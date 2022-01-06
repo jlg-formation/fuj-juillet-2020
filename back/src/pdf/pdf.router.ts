f2f18f66e3d41fce17108cc105e91e5bc2ac0011
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-06 20:07:24 +0100

    dev: added pdf router with example

--- back/src/pdf/pdf.router.ts
+++ back/src/pdf/pdf.router.ts
@@ -1,9 +1,33 @@
 import {Router} from 'express';
+import puppeteer from 'puppeteer';
 
 const app = Router();
 
 app.post('/article', (req, res) => {
-  res.status(201).sendFile('./temp/test.pdf', {root: process.cwd()});
+  (async () => {
+    try {
+      const pdf = await printPDF();
+      res.set({
+        'Content-Type': 'application/pdf',
+        'Content-Length': pdf.length,
+      });
+      res.status(201).send(pdf);
+    } catch (err) {
+      res.status(500).end();
+    }
+  })();
 });
 
 export const pdfRouter = app;
+
+async function printPDF() {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/legal', {
+    waitUntil: 'networkidle0',
+  });
+  const pdf = await page.pdf({format: 'a4'});
+
+  await browser.close();
+  return pdf;
+}
