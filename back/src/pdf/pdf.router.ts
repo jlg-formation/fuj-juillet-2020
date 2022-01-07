4cb733befea20e336ea0001da3312b4582b5e00d
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-07 11:11:55 +0100

    dev: pdf working

--- back/src/pdf/pdf.router.ts
+++ back/src/pdf/pdf.router.ts
@@ -1,8 +1,10 @@
 import ejs from 'ejs';
 import {Router} from 'express';
+import {basename} from 'path';
 import puppeteer from 'puppeteer';
 import {assert} from 'superstruct';
 import {ArticleModel} from './../validation/article.model';
+import fs from 'fs';
 
 const app = Router();
 
@@ -24,10 +26,29 @@ app.post('/article', (req, res) => {
 
 export const pdfRouter = app;
 
+async function getImageContent(url: string) {
+  console.log('url: ', url);
+  const imgFilename = './uploads/' + basename(url);
+  console.log('imgFilename: ', imgFilename);
+  const buffer = await fs.promises.readFile(imgFilename);
+  console.log('buffer: ', buffer);
+  return 'data:image/jpeg;base64,' + buffer.toString('base64');
+}
+
 async function printPDF(article: unknown) {
   assert(article, ArticleModel);
-  const html = await ejs.renderFile('./ejs/article.ejs', {article});
-  console.log('html: ', html);
+  const images = [];
+  for (const url of article.images) {
+    const imgData = await getImageContent(url);
+    images.push(imgData);
+  }
+  const cssFileContent = await fs.promises.readFile('./ejs/article.css');
+  const css = `<style>${cssFileContent}</style>`;
+  const html = await ejs.renderFile('./ejs/article.ejs', {
+    article,
+    images,
+    css,
+  });
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.setContent(html, {
