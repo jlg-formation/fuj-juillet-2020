fe20e720eec7399e1632c27f489f9016bd45e207
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-14 12:04:05 +0100

    dev: article details

--- front/src/app/stock/article/article.component.ts
+++ front/src/app/stock/article/article.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { ArticleService } from './../../services/article.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ArticleComponent {
+  article!: Article;
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {
+    this.route.paramMap.subscribe((map) => {
+      console.log('map: ', map);
+      const id = map.get('id');
+      if (!id) {
+        return;
+      }
+      const article = this.articleService.documents$.value.find(
+        (a) => a.id === id
+      );
+      if (!article) {
+        return;
+      }
+      this.article = article;
+    });
   }
-
 }
