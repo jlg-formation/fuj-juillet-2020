5be00e404a9e674d05af4a80d315c87c4d88d175
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-16 18:52:29 +0100

    dev: upgrade angular and update

--- front/src/app/stock/update/update.component.ts
+++ front/src/app/stock/update/update.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { ArticleService } from './../../services/article.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  DuplicateAsyncValidator,
+  JlgValidators,
+} from '@jlguenego/angular-tools';
+import { faPen, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
-  styleUrls: ['./update.component.scss']
+  styleUrls: ['./update.component.scss'],
 })
-export class UpdateComponent implements OnInit {
+export class UpdateComponent {
+  f = new FormGroup({
+    name: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.maxLength(20),
+        Validators.minLength(3),
+      ],
+    }),
+    price: new FormControl(undefined, [Validators.required]),
+    qty: new FormControl(undefined, [
+      Validators.required,
+      JlgValidators.integer,
+    ]),
+  });
+  faCircleNotch = faCircleNotch;
+  faPen = faPen;
+  isUpdating = false;
 
-  constructor() { }
+  constructor(
+    private duplicateAsyncValidator: DuplicateAsyncValidator,
+    private articleService: ArticleService
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  submit() {}
 }
