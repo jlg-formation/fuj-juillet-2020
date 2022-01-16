3593a5aa98d3d4699366398c91b6d7621540076a
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-16 15:39:04 +0100

    dev: added google

--- front/src/app/user/login/login.component.ts
+++ front/src/app/user/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { faGithub, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
+import {
+  faGithub,
+  faGoogle,
+  faMicrosoft,
+} from '@fortawesome/free-brands-svg-icons';
 import { faSmileWink } from '@fortawesome/free-regular-svg-icons';
 import { Oauth2Service } from '@jlguenego/angular-tools';
 
@@ -11,6 +15,8 @@ import { Oauth2Service } from '@jlguenego/angular-tools';
 export class LoginComponent {
   faGithub = faGithub;
   faMicrosoft = faMicrosoft;
+  faGoogle = faGoogle;
+
   faSmileWink = faSmileWink;
   constructor(public oauth2Service: Oauth2Service) {}
 }
