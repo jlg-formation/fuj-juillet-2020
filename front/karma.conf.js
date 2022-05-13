778a86110652c53d3f7ec109bd77389c622c2cbe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-13 17:15:16 +0200

    dev: upgraded to angular 13.3

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -26,7 +26,6 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
       dir: require('path').join(__dirname, './coverage/front'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
